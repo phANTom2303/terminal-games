6929a938510f8746eaa0bebf661875085531af93
phANTom2303 <anikavy.goenka@gmail.com>
2024-06-04 10:57:02 +0530

    Implemented a function to spawn new tiles

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <random>
+
 using namespace std;
 
 vector<vector<int>> board = {{0, 0, 0, 0},
-                             {0, 2, 0, 0},
-                             {2, 0, 2048, 0},
-                             {0, 2, 0, 0}};
+                             {0, 0, 0, 0},
+                             {0, 0, 0, 0},
+                             {0, 0, 0, 0}};
 
 string topLine = " ┌──────┬──────┬──────┬──────┐";
 string midLine = " ├──────┼──────┼──────┼──────┤";
@@ -22,6 +24,31 @@ string getNumberSpacing(int n)
         res = " " + res;
     return res;
 }
+
+void spawnNumber()
+{
+    // AI Generated Code begins
+    random_device rd;                       // obtain a random seed
+    mt19937 gen(rd());                      // seed the random number generator
+    uniform_int_distribution<> dis_i(0, 3); 
+    uniform_int_distribution<> dis_j(0, 3);
+    uniform_int_distribution<> dis_odds(1, 10);
+    int i = dis_i(gen);
+    int j = dis_j(gen);
+    int odds = dis_odds(gen);
+    // AI Generated code ends
+
+    if(board[i][j] == 0)
+    {
+        int num = (odds<=9)?2:4;
+        board[i][j] = num;
+    }
+    else
+        spawnNumber();
+    
+}
+
+
 void render()
 {
 
